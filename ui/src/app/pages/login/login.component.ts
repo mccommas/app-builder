1b083df21e100174545bd168f7ec00f3986a4b5c
Shashwat Tripathi <stripathix@gmail.com>
2016-12-02 22:32:03 +0530

    Login logic

--- ui/src/app/pages/login/login.component.ts
+++ ui/src/app/pages/login/login.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Transition } from 'ui-router-ng2';
 import { LoginService } from './login.service';
 import { HttpService } from './../../shared/services/http.service';
 import { ConstantService } from './../../shared/services/constant.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService, HttpService, ConstantService]
 })
 export class LoginComponent implements OnInit {
-
-  constructor(private loginService: LoginService) { }
+  public userPassword: any;
+  constructor(private loginService: LoginService, private transition: Transition) { }
 
   ngOnInit() {
   }
-  login() { 
-    alert("Doing login")
+  login(model, isValid) {
+    this.loginService.doLogin().map(r => r.json())
+      .subscribe(data => {
+        if (data) {
+          if (data.email == model.useremail && data.password == model.userpassword) {
+            this.transition.router.stateService.go('dashboard');
+          } else {
+            alert("Invalid Credentials");
+          }
+        }
+      });
   }
 
 }
